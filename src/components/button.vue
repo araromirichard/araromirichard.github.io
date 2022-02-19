dbdc28bacb93f7c35131d64a7e97390911e24b83
karosaxy <araromirichard@gmail.com>
2022-02-19 12:27:51 +0100

    responsive btn

--- src/components/button.vue
+++ src/components/button.vue
@@ -2,8 +2,7 @@
   <div>
     <v-hover v-slot="{ hover }">
       <v-btn
-      v-if="$vuetify.breakpoint.mdAndUp"
-        x-large
+        v-if="$vuetify.breakpoint.mdAndUp ? 'x-large':'small'"
         class="ma-2"
         rounded2
         outlined
@@ -16,21 +15,7 @@
       >
         {{ btnName }}
       </v-btn>
-      <v-btn
-      v-if="$vuetify.breakpoint.smAndDown"
-        
-        class="ma-2"
-        rounded2
-        outlined
-        color="#FAFAFA"
-        :style="{
-          'background-color': hover ? '#ba1c46' : '',
-          border: hover ? 'none' : '',
-          color: hover ? 'white' : '#fafafa',
-        }"
-      >
-        {{ btnName }}
-      </v-btn>
+     
     </v-hover>
   </div>
 </template>
@@ -40,6 +25,11 @@ export default {
   name: 'mybutton',
   props: {
     btnName: String
+  },
+  data(){
+    return {
+      //
+    }
   }
 }
 </script>
